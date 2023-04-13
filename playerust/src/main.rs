8eccea9ca018b33a225cedb3b994413be0e311cb
Night_Hunter <samuelhuntnz@gmail.com>
2023-04-13 12:56:09 +1200

    add more dubuging

--- playerust/src/main.rs
+++ playerust/src/main.rs
@@ -14,7 +14,7 @@ use update::{update, Message};
 use view::view;
 
 fn main() {
-    // std::env::set_var("GST_DEBUG", "3");
+    std::env::set_var("GST_DEBUG", "3");
     env_logger::init();
     State::run(Default::default()).unwrap();
 }
